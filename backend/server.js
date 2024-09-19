2ac0b78cee277aabf5c505886fa15baf01d616d8
LuuAiGiao <luugiao.lm2003@gmail.com>
2024-09-19 13:33:26 +0700

    commit backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const database = require("./config/db");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const port = 3000;
+
+database.connect();
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+// app.use("/", router);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
