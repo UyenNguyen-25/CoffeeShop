7968ef101619986006a5e90c06872abb0766f727
UyenNguyen-25 <khoang8666@gmail.com>
2024-09-20 13:50:46 +0700

    update layout

--- frontend/src/auth/AnonymousLayout.jsx
+++ frontend/src/auth/AnonymousLayout.jsx
@@ -6,9 +6,9 @@ const AnonymousLayout = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen relative">
-      <div className="h-full bg-login-bg bg-cover bg-center bg-no-repeat w-full opacity-70 absolute z-0"></div>
+    <div className="h-screen bg-login-bg bg-cover bg-center bg-no-repeat">
       {/* Main Content */}
+      <div className="h-full w-full bg-black opacity-50 z-0 absolute"></div>
       <div className="flex h-full flex-col items-center overflow-y-auto rounded-md">
         <div className="w-full font-mono z-40 p-8">
           <Link
@@ -20,7 +20,7 @@ const AnonymousLayout = () => {
         </div>
         {/* Dynamic Route Content */}
         <main className="h-[75%] z-50 flex items-center">
-          <div className="bg-white shadow-2xl min-w-[400px] max-w-[450px] h-fit p-6 pb-2 rounded-lg">
+          <div className="bg-white min-w-[400px] max-w-[450px] h-fit p-6 pb-2 rounded-lg">
             <Outlet />
           </div>
         </main>
