5b3a7c7c544760e31b13d175009f8c156b797a42
UyenNguyen-25 <khoang8666@gmail.com>
2024-10-04 12:33:45 +0700

    coffee-mixer done

--- frontend/src/pages/ShoppingPages/MixCoffeePage/index.jsx
+++ frontend/src/pages/ShoppingPages/MixCoffeePage/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Select, Slider, Button, Row, Col, Card, Typography, message, List } from 'antd';
 import { coffeeOptions, coffeeSuggestions } from '@/constant/CoffeeSuggestions';
 
@@ -7,9 +7,11 @@ const { Title, Paragraph } = Typography;
 
 const CoffeeMixer = () => {
     const [selectedCoffees, setSelectedCoffees] = useState([]);
+    const [selectedSuggestion, setSelectedSuggestion] = useState(null);
     const [ratios, setRatios] = useState({ arabica: 0, robusta: 0, culi: 0 });
     const [description, setDescription] = useState('');
     const [total, setTotal] = useState(0)
+    const sectionRef = useRef(null);
 
     const handleCoffeeChange = (value) => {
         setSelectedCoffees(value);
@@ -53,16 +55,19 @@ const CoffeeMixer = () => {
         setSelectedCoffees([]);
     };
 
+
+    const scrollToSection = () => {
+        sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
     const handleSelectSuggestions = (suggestion) => {
-        resetRatios();
-        console.log(suggestion.ratio);
         const filteredRatiosKey = Object.keys(suggestion.ratio)
             .filter(key => suggestion.ratio[key] !== 0);
-
-        console.log(filteredRatiosKey);
-
+        setSelectedSuggestion(suggestion.name);
+        resetRatios();
         setSelectedCoffees(filteredRatiosKey)
         setRatios(suggestion.ratio)
+        scrollToSection();
     }
 
     const sortCoffeeRatios = (coffeeRatios) => {
@@ -79,158 +84,175 @@ const CoffeeMixer = () => {
 
     return (
         <div className="container my-10">
-            <Title level={2} style={{ textAlign: 'center' }}>Bạn có thể sẽ thích</Title>
-            <Row gutter={[16, 16]} justify="center" style={{ display: 'flex', flexWrap: 'wrap' }}>
-                {coffeeSuggestions.map((suggestion, index) => {
-                    return (
-                        <Col xs={24} sm={12} lg={8} key={index} style={{ display: 'flex' }}>
-                            <Card title={suggestion.name}
-                                onClick={() => handleSelectSuggestions(suggestion)}
-                                style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                                <p>{suggestion.description}</p>
-                                <p className='font-semibold mt-3'>Tỷ lệ: </p>
-                                {sortCoffeeRatios(suggestion.ratio)}
+            <div>
+                <Title level={2} style={{ textAlign: 'center' }}>Bạn có thể sẽ thích</Title>
+                <Row gutter={[16, 16]} justify="center" style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {coffeeSuggestions.map((suggestion, index) => {
+                        const isSelected = selectedSuggestion === suggestion.name;
+                        return (
+                            <Col xs={24} sm={12} lg={8} key={index} style={{ display: 'flex' }}>
+                                <Card title={suggestion.name}
+                                    onClick={() => handleSelectSuggestions(suggestion)}
+                                    style={{
+                                        flex: 1,
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'space-between',
+                                        backgroundColor: isSelected ? '#B28A62' : '',
+                                        color: isSelected ? 'white' : 'black',
+                                        cursor: 'pointer'
+                                    }}
+                                    styles={{
+                                        header: { color: isSelected ? 'white' : 'black' }
+                                    }}>
+                                    <p>{suggestion.description}</p>
+                                    <p className='font-semibold mt-3'>Tỷ lệ: </p>
+                                    {sortCoffeeRatios(suggestion.ratio)}
+                                </Card>
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </div>
+
+            {/* Custom Ratios */}
+            <div className='mt-10 pt-4' ref={sectionRef}>
+                <Title level={2} style={{ textAlign: 'center' }}>Tự do sáng tạo</Title>
+
+                {/* Chọn Chủng Loại */}
+                <Row justify="center" style={{ marginBottom: '20px', marginTop: '30px' }}>
+                    <Col xs={24} sm={12} lg={8}>
+                        <Select
+                            mode="multiple"
+                            allowClear
+                            placeholder="Chọn loại cà phê"
+                            value={selectedCoffees}
+                            onChange={handleCoffeeChange}
+                            style={{ width: '100%' }}
+                            maxTagCount={3}
+                        >
+                            {coffeeOptions.map(option => (
+                                <Option key={option.value} value={option.value}>
+                                    {option.label}
+                                </Option>
+                            ))}
+                        </Select>
+                    </Col>
+                </Row>
+
+                {/* Điều Chỉnh Tỉ Lệ */}
+                <Row gutter={[16, 16]} justify="center">
+                    {selectedCoffees.includes('arabica') && (
+                        <Col xs={24} sm={12} md={6}>
+                            <Card title="Arabica" bordered>
+                                <Slider
+                                    min={0}
+                                    max={100}
+                                    value={ratios.arabica}
+                                    onChange={(value) => handleSliderChange(value, 'arabica')}
+                                />
+                                <p>Tỉ lệ: {ratios.arabica}%</p>
                             </Card>
                         </Col>
-                    )
-                })}
-            </Row>
+                    )}
+                    {selectedCoffees.includes('robusta') && (
+                        <Col xs={24} sm={12} md={6}>
+                            <Card title="Robusta" bordered>
+                                <Slider
+                                    min={0}
+                                    max={100}
+                                    value={ratios.robusta}
+                                    onChange={(value) => handleSliderChange(value, 'robusta')}
+                                />
+                                <p>Tỉ lệ: {ratios.robusta}%</p>
+                            </Card>
+                        </Col>
+                    )}
+                    {selectedCoffees.includes('culi') && (
+                        <Col xs={24} sm={12} md={6}>
+                            <Card title="Culi" bordered>
+                                <Slider
+                                    min={0}
+                                    max={100}
+                                    value={ratios.culi}
+                                    onChange={(value) => handleSliderChange(value, 'culi')}
+                                />
+                                <p>Tỉ lệ: {ratios.culi}%</p>
+                            </Card>
+                        </Col>
+                    )}
+                </Row>
 
-            <Title level={2} style={{ textAlign: 'center' }}>Tự do sáng tạo</Title>
-
-            {/* Chọn Chủng Loại */}
-            <Row justify="center" style={{ marginBottom: '20px' }}>
-                <Col xs={24} sm={12} lg={8}>
-                    <Select
-                        mode="multiple"
-                        allowClear
-                        placeholder="Chọn loại cà phê"
-                        value={selectedCoffees}
-                        onChange={handleCoffeeChange}
-                        style={{ width: '100%' }}
-                        maxTagCount={3}
-                    >
-                        {coffeeOptions.map(option => (
-                            <Option key={option.value} value={option.value}>
-                                {option.label}
-                            </Option>
-                        ))}
-                    </Select>
-                </Col>
-            </Row>
-
-            {/* Điều Chỉnh Tỉ Lệ */}
-            <Row gutter={[16, 16]} justify="center">
-                {selectedCoffees.includes('arabica') && (
-                    <Col xs={24} sm={12} md={6}>
-                        <Card title="Arabica" bordered>
-                            <Slider
-                                min={0}
-                                max={100}
-                                value={ratios.arabica}
-                                onChange={(value) => handleSliderChange(value, 'arabica')}
-                            />
-                            <p>Tỉ lệ: {ratios.arabica}%</p>
+                {/* Mô tả kết quả */}
+                <Row justify="center" gutter={[16, 16]} style={{ marginTop: '20px' }}>
+                    <Col xs={24} sm={12} lg={8}>
+                        <Card>
+                            <Paragraph>{
+                                description.length > 0 ? (
+                                    <><List
+                                        header={<div><strong>Công thức của bạn gồm có:</strong></div>}
+                                        bordered={false}
+                                        dataSource={description}
+                                        renderItem={(item) => (
+                                            <List.Item>
+                                                <Typography.Text>• {item}</Typography.Text>
+                                            </List.Item>
+                                        )}
+                                    />
+                                        {total !== 100 && "Còn thiếu: " + (100 - total) + "%"}
+                                    </>
+                                ) : 'Hãy điều chỉnh tỉ lệ để xem mô tả phù hợp.'}</Paragraph>
                         </Card>
                     </Col>
-                )}
-                {selectedCoffees.includes('robusta') && (
-                    <Col xs={24} sm={12} md={6}>
-                        <Card title="Robusta" bordered>
-                            <Slider
-                                min={0}
-                                max={100}
-                                value={ratios.robusta}
-                                onChange={(value) => handleSliderChange(value, 'robusta')}
-                            />
-                            <p>Tỉ lệ: {ratios.robusta}%</p>
-                        </Card>
-                    </Col>
-                )}
-                {selectedCoffees.includes('culi') && (
-                    <Col xs={24} sm={12} md={6}>
-                        <Card title="Culi" bordered>
-                            <Slider
-                                min={0}
-                                max={100}
-                                value={ratios.culi}
-                                onChange={(value) => handleSliderChange(value, 'culi')}
-                            />
-                            <p>Tỉ lệ: {ratios.culi}%</p>
-                        </Card>
-                    </Col>
-                )}
-            </Row>
-
-            {/* Mô tả kết quả */}
-            <Row justify="center" gutter={[16, 16]} style={{ marginTop: '20px' }}>
-                <Col xs={24} sm={12} lg={8}>
-                    <Card>
-                        <Paragraph>{
-                            description.length > 0 ? (
-                                <><List
-                                    header={<div><strong>Công thức của bạn gồm có:</strong></div>}
+                    <Col xs={24} sm={12} lg={8}>
+                        <Card>
+                            <Paragraph>
+                                <List
+                                    header={<div><strong>Thành Tiền</strong></div>}
                                     bordered={false}
-                                    dataSource={description}
+                                    dataSource={[
+                                        {
+                                            title: "Tiền Hạt",
+                                            amount: "50,000 VND", // Số tiền hạt
+                                        },
+                                        {
+                                            title: "Tiền Xay",
+                                            amount: "10,000 VND", // Số tiền xay
+                                        },
+                                        {
+                                            title: "Tổng Cộng",
+                                            amount: "60,000 VND", // Tổng cộng tiền
+                                        },
+                                    ]}
                                     renderItem={(item) => (
                                         <List.Item>
-                                            <Typography.Text>• {item}</Typography.Text>
+                                            <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                                                <span>{item.title}</span>
+                                                <span>{item.amount}</span>
+                                            </div>
                                         </List.Item>
                                     )}
                                 />
-                                    {total !== 100 && "Còn thiếu: " + (100 - total) + "%"}
-                                </>
-                            ) : 'Hãy điều chỉnh tỉ lệ để xem mô tả phù hợp.'}</Paragraph>
-                    </Card>
-                </Col>
-                <Col xs={24} sm={12} lg={8}>
-                    <Card>
-                        <Paragraph>
-                            <List
-                                header={<div><strong>Thành Tiền</strong></div>}
-                                bordered={false}
-                                dataSource={[
-                                    {
-                                        title: "Tiền Hạt",
-                                        amount: "50,000 VND", // Số tiền hạt
-                                    },
-                                    {
-                                        title: "Tiền Xay",
-                                        amount: "10,000 VND", // Số tiền xay
-                                    },
-                                    {
-                                        title: "Tổng Cộng",
-                                        amount: "60,000 VND", // Tổng cộng tiền
-                                    },
-                                ]}
-                                renderItem={(item) => (
-                                    <List.Item>
-                                        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                                            <span>{item.title}</span>
-                                            <span>{item.amount}</span>
-                                        </div>
-                                    </List.Item>
-                                )}
-                            />
-                            {total !== 100 && "Còn thiếu: " + (100 - total) + "%"}
-                        </Paragraph>
-                    </Card>
-                </Col>
-            </Row>
+                                {total !== 100 && "Còn thiếu: " + (100 - total) + "%"}
+                            </Paragraph>
+                        </Card>
+                    </Col>
+                </Row>
 
-            {/* Button Confirm và Reset */}
-            <Row justify="center" gutter={[16, 16]} style={{ marginTop: '20px' }}>
-                <Col>
-                    <Button type="primary" onClick={handleConfirm}>
-                        Thanh Toán
-                    </Button>
-                </Col>
-                <Col>
-                    <Button onClick={handleReset}>
-                        Làm Mới
-                    </Button>
-                </Col>
-            </Row>
+                {/* Button Confirm và Reset */}
+                <Row justify="center" gutter={[16, 16]} style={{ marginTop: '20px' }}>
+                    <Col>
+                        <Button type="primary" onClick={handleConfirm}>
+                            Thanh Toán
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={handleReset}>
+                            Làm Mới
+                        </Button>
+                    </Col>
+                </Row>
+            </div>
         </div>
     );
 };
