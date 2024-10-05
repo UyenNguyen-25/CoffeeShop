3addd7aab414cb966e2267bb9c2dbdddff9b65b6
UyenNguyen-25 <khoang8666@gmail.com>
2024-10-05 17:39:17 +0700

    set  up cors

--- frontend/src/constants/apiConfig.jsx
+++ frontend/src/constants/apiConfig.jsx
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://con-yeu-g5.vercel.app";
-export const BASE_URL = 'http://localhost:3000/'
+export const BASE_URL = "https://coffeeshopbe.vercel.app/";
+// export const BASE_URL = 'http://localhost:3000/'
 
